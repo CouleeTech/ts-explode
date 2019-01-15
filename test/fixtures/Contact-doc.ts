64c6a3d918f0f00ae0097c145211ac4eb8bc6277
Isaac Weaver <isaac@couleetechlink.com>
2019-01-15 09:35:14 -0600

    test: add simple test

---
+++ test/fixtures/Contact-doc.ts
@@ -0,0 +1,16 @@
+export interface Contact {
+    id: string;
+    tags: Array< {
+            name: string,
+        }
+        >;
+    phones: Array< {
+            type: | PhoneType.CELL
+            | PhoneType.BUSINESS
+            | PhoneType.HOME
+            ,
+            number: number,
+        }
+        >;
+    name: string;
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,19 +1,19 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from '@oclif/test'
 
 import cmd = require('../src')
+import { tmpdir } from 'os'
+import { readFileSync } from 'fs'
+import { join } from 'path'
 
-describe('interface-doc', () => {
-  test
-    .stdout()
-    .do(() => cmd.run([]))
-    .it('runs hello', ctx => {
-      expect(ctx.stdout).to.contain('hello world')
-    })
+describe('ts-explode', () => {
+  const dir = tmpdir()
 
   test
     .stdout()
-    .do(() => cmd.run(['--name', 'jeff']))
-    .it('runs hello --name jeff', ctx => {
-      expect(ctx.stdout).to.contain('hello jeff')
+    .do(() => cmd.run(['--out', dir, 'test/testInterfaces/']))
+    .it('generates correct file', ctx => {
+      expect(readFileSync(join(dir, 'Contact-doc.ts'), 'utf8')).to.equal(
+        readFileSync('test/fixtures/Contact-doc.ts', 'utf8')
+      )
     })
 })
